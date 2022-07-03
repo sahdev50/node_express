67b6a3d017893afbc7e76e28cbee31e43dfeda55
sahdev50 <dxdestroyer007@gmail.com>
2022-07-03 17:08:07 +0100

    adding templating engine EJS

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const path = require('path')
 const app = express()
+
+// serving html pages with ejs
+app.set('view engine','ejs')
+app.set('views', path.join(__dirname,'views'))
+
+// serving static files(css, js or images)
+app.use(express.static(path.join(__dirname,'public')))
 
 // routers
 const adminRouter = require('./routes/admin')
-
 
 app.use(bodyParser.urlencoded({extended:false}))
 
--- routes/admin.js
+++ routes/admin.js
@@ -3,8 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/add-product', (req, res, next)=>{
-    res.send("<html><body><h1>Add Product</h1><form action='/products' method='POST' ><input type='text' name='product_name' placeholder='enter name...' /><button type='submit'>Submit</button></form></body></html>")
-})
+    res.render('admin.ejs',{
+        path:'/add-product',
+        pageTitle:'admin'
+    })
+    })
 
 router.post('/products',(req, res, next)=>{
     console.log(req.body.product_name)
