3e7fd27a623585533e66e2a6bd1ab184e291f318
Sahdev <dxdestroyer007@gmail.com>
2022-08-04 02:04:15 +0100

    add passport-config.js file

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express')
-
+const path = require('path')
+const bodyParser = require('body-parser')
 // initialize app
 const app = express()
 
+// set ejs
+app.set('view engine', 'ejs')
+
+app.use(express.static(path.join(__dirname,'public')))
+app.use(bodyParser.urlencoded({ extended: false }))
+
 app.get('/', (req, res, next)=>{
-    res.send('<h1>Hello World</h1>')
+    res.render('index', {
+        name:'sahdev'
+    })
+})
+app.post('/get_form',(req, res, next)=>{
+    console.log(req.body.name)
+    res.redirect('/')
 })
 
 app.listen(3000, ()=>{
